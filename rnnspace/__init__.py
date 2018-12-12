fc18c34165b27cd2e78940b51d2e85a7241c11a4
lovit <soy.lovit@gmail.com>
2018-12-12 20:59:53 +0900

    Commit vocabulary scanner, encoder & decoder of item

--- rnnspace/__init__.py
+++ rnnspace/__init__.py
@@ -1,2 +1,5 @@
 from .dataset import load_lalaland
 from .utils import space_tag
+from .utils import scan_vocabulary
+from .utils import to_idx
+from .utils import to_item
--- rnnspace/utils.py
+++ rnnspace/utils.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import os
 
 installpath = os.path.sep.join(
@@ -35,3 +36,60 @@ def space_tag(sent, nonspace=0, space=1):
             idx += 1
 
     return chars, tags
+
+def scan_vocabulary(texts, min_count=1):
+    """
+    :param texts: list of str
+        Input sentences
+    :param min_count: int
+        Minimum frequency of vocabulary occurrence. Default is 1
+
+    It returns
+    ----------
+    idx_to_vocab: list of str
+    vocab_to_idx: dict, str to int
+    """
+
+    counter = Counter(vocab for text in texts for vocab in text)
+    counter = {vocab:count for vocab, count in counter.items() if count >=min_count}
+    idx_to_vocab = [vocab for vocab in sorted(counter, key=lambda x:-counter[x])]
+    vocab_to_idx = {vocab:idx for idx, vocab in enumerate(idx_to_vocab)}
+    return idx_to_vocab, vocab_to_idx
+
+def to_idx(item, mapper, unknown=None):
+    """
+    :param item: Object
+        Object to be encoded
+    :param mapper: dict
+        Dictionary from item to idx
+    :param unknown: int
+        Index of unknown item. If None, use len(mapper)
+
+    It returns
+    ----------
+    idx : int
+        Index of item
+    """
+
+    if unknown is None:
+        unknown = len(mapper)
+    return mapper.get(item, unknown)
+
+def to_item(idx, idx_to_vocab, unknown=None):
+    """
+    :param idx: int
+        Index of item
+    :param idx_to_vocab: list of Object
+        Mapper from index to item object
+    :param unknown: Object
+        Return value when the idx is outbound of idx_to_vocab
+
+    It returns
+    ----------
+    object : object
+        Item that corresponding idx
+    """
+
+    if 0 <= idx < len(idx_to_vocab):
+        return idx_to_vocab[idx]
+    return unknown
